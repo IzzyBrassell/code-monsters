38f569dea4f4e9d4df106a93d58c58b0987b02b5
Brian Chiang <kyaerorgabootcamp@gmail.com>
2023-04-12 13:44:51 -0400

    working pages

--- server/routes/home/homepage-routes.js
+++
@@ -1,9 +0,0 @@
-const router = require(`express`).Router()
-
-router
-    .route(`/`)
-    .get((req,res) => {
-        res.send(`${__dirname}/../client/build`)
-    })
-
-module.exports = router
--- server/server.js
+++ server/server.js
@@ -3,9 +3,6 @@ const app = express()
 const PORT = process.env.PORT || 3001
 const path = require('path')
 const session = require(`express-session`)
-=========
-const path = require('path')
->>>>>>>>> Temporary merge branch 2
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -18,10 +15,7 @@ app.use(session({
   }))
 app.use(require(`./routes`))
 
-const reactRoutes = ['/', '/Login', '/CC', '/DS', '/S1', '/S2a', '/S2b', '/S2c', '/S2d', '/S2e', '/S3a', '/S3b', '/S3c', '/S3d', '/S3e', '/S4a', '/S4b', '/S4c', '/S4d', '/S4e', '/S5a', '/S5b', '/S5c', '/S5d', '/S5e', '/S6a', '/S6b', '/S6c', '/S6d', '/S6e', '/S7a', '/S7b']; 
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-app.get(reactRoutes, (req, res) => {
+app.get(`*`, (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
