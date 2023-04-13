81be31981fe8c6453724d20b387cedbe1b57bb40
Israel Brassell <israel.brassell@gmail.com>
2023-04-13 17:29:19 -0400

    do not pull yet

--- client/src/pages/characterCreation/index.js
+++ client/src/pages/characterCreation/index.js
@@ -1,10 +1,92 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form'
+import { useHistory } from 'react-router-dom';
 
-function CharacterCreation() {
-//Need Post logic for Submit button that also immedietly sends player to /S1
+function CharacterCreation({ userid }) {
+    const history = useHistory();
+  
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+      
+        try {
+          const response = await fetch(`/api/characters?userid=${userid}`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+      
+          if (response.ok) {
+            const character = await response.json();
+            if (character) {
+              // Character already exists, send a PUT request to update
+              try {
+                const updateResponse = await fetch('/api/characters', {
+                  method: 'PUT',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  // Replace with your form data, including the userid from props
+                  body: JSON.stringify({
+                    name: e.target.name.value,
+                    gender: e.target.gender.value,
+                    ability: e.target.ability.value,
+                    trait: e.target.trait.value,
+                    userid: userid,
+                  }),
+                });
+      
+                if (updateResponse.ok) {
+                  // Handle successful response
+                  history.push('/S1');
+                } else {
+                  // Handle error response
+                  console.error('Character Update Failed:', updateResponse.statusText);
+                }
+              } catch (error) {
+                console.error(error);
+              }
+            } else {
+              // Character doesn't exist, send a POST request to create
+              try {
+                const createResponse = await fetch('/api/characters', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  // Replace with your form data, including the userid from props
+                  body: JSON.stringify({
+                    name: e.target.name.value,
+                    gender: e.target.gender.value,
+                    ability: e.target.ability.value,
+                    trait: e.target.trait.value,
+                    userid: userid,
+                  }),
+                });
+      
+                if (createResponse.ok) {
+                  // Handle successful response
+                  history.push('/S1');
+                } else {
+                  // Handle error response
+                  console.error('Character Creation Failed:', createResponse.statusText);
+                }
+              } catch (error) {
+                console.error(error);
+              }
+            }
+          } else {
+            // Handle error response
+            console.error('Error fetching character:', response.statusText);
+          }
+        } catch (error) {
+          // Handle fetch error
+          console.error(error);
+        }
+      };
+
     return(
-        <Form>
+        <Form onSubmit={handleFormSubmit}>
             <Form.Group>
                 <Form.Label>What is our Hero's Name?</Form.Label>
                 <Form.Control type='name' placeholder='Enter Name' />
@@ -36,7 +118,7 @@ function CharacterCreation() {
                 <option value="3">Frail (Hard Mode)</option>
                 </Form.Select>
             </Form.Group>
-            {/* <Button variant="Secondary" type="submit">Start the Adventure</Button> */}
+            <Button variant="Secondary" type="submit">Start the Adventure</Button>
         </Form>
     );
 }
